c8fdafb609f8af831e0168109748cbaad1a163e7
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-09 18:56:04 +0800

    Update siwebringuservars.js

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -2,4 +2,4 @@ var name = "MECHAGIC";
 var thing = "having a joyous PLATONIC time";
 var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
-var image = "https://dears.nekoweb.org/siwebring/siblank.png"
+var image = "https://mechagic.party/2images/3tjarparchivist.webp"
