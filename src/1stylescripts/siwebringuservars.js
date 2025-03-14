55b2503fb9722ba862e867819ccca9ff7df76fe1
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 19:53:58 +0800

    Update siwebringuservars.js

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -3,3 +3,5 @@ var thing = "having a joyous PLATONIC time";
 var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
 var image = "https://mechagic.party/2images/3tjarparchivist.webp"
+
+$("templateWebring").css("width","85%");
