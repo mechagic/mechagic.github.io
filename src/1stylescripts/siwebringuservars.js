d5eabae904dfc7f0b49536bf2ae771eeafb06eeb
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 20:16:56 +0800

    Update siwebringuservars.js

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -7,3 +7,5 @@ var image = "https://mechagic.party/2images/3tjarparchivist.webp"
 const templateWebring = document.querySelector('templateWebring');
 templateWebring.style.width = "85%";
 templateWebring.style.height = "auto";
+document.querySelector('#siwebring #templateWebring').style = 
+"    font-family: trebuchet ms, sans-serif;    text-align: center;    margin:auto;    width: 85%!important;    height: auto!important;    background: url('/siwebring/siwidgetbg.png') no-repeat;    background-size: cover;    border: 3px #FF74B8 solid;    font-size: 16px;"
