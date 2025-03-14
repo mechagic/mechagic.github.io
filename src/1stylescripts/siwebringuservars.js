e23e536576dd57ceb65c2e5ac53a4e75101c9d7d
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 21:00:10 +0800

    everybody wants to be a super organism

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -5,6 +5,6 @@ var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
 var image = "https://mechagic.party/2images/3tjarparchivist.webp";
 
-/*webring css*/
+/*webring custom css*/
 $("templateWebring").css("width","85%"),
 $("templateWebring").css("height","auto");
