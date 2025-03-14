9286a70a9536538b9b2b91990cd00446ed7307a9
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 20:22:02 +0800

    Update siwebringuservars.js

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -4,8 +4,4 @@ var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
 var image = "https://mechagic.party/2images/3tjarparchivist.webp"
 
-const templateWebring = document.querySelector('templateWebring');
-templateWebring.style.width = "85%";
-templateWebring.style.height = "auto";
-document.querySelector('#siwebring #templateWebring').style = 
-"    font-family: trebuchet ms, sans-serif;    text-align: center;    margin:auto;    width: 85%!important;    height: auto!important;    background: url('/siwebring/siwidgetbg.png') no-repeat;    background-size: cover;    border: 3px #FF74B8 solid;    font-size: 16px;"
+$("templateWebring").css("width","85%"), $("templateWebring").css("height","auto");
