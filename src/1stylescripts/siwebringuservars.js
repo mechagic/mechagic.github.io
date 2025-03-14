6cfb745b341d477028b06a1d3cd9850d5a2d91a7
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 20:58:43 +0800

    Update siwebringuservars.js

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -1,8 +1,10 @@
+/*webring variables*/
 var name = "MECHAGIC";
 var thing = "having a joyous PLATONIC time";
 var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
 var image = "https://mechagic.party/2images/3tjarparchivist.webp";
 
+/*webring css*/
 $("templateWebring").css("width","85%"),
 $("templateWebring").css("height","auto");
