672e9052d35f14ac275dfbe96cf3046bac97b866
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 20:43:15 +0800

    i am so smart (not really, im an idiot)

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -2,8 +2,6 @@ var name = "MECHAGIC";
 var thing = "having a joyous PLATONIC time";
 var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
-var image = "https://mechagic.party/2images/3tjarparchivist.webp"
+var image = "https://mechagic.party/2images/3tjarparchivist.webp";
 
-document.querySelector('#siwebring #templateWebring').style = "font-family: trebuchet ms, sans-serif;     text-align: center;     margin:auto;     width: 85%!important;     height: auto!important;     background: url('/siwebring/siwidgetbg.png') no-repeat;     background-size: cover;     border: 3px #FF74B8 solid;     font-size:%2016px;"
-$("templateWebring").css("width","85%"),
-$("templateWebring").css("height","auto");
+document.querySelector('#siwebring #templateWebring').style = "font-family: trebuchet ms, sans-serif;     text-align: center;     margin:auto;     width: 85%!important;     height: auto!important;     background: url('/siwebring/siwidgetbg.png') no-repeat;     background-size: cover;     border: 3px #FF74B8 solid;     font-size:%2016px;";
