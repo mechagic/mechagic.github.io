e6e5327ccff2cd4de119014f2b53b6517d62e209
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 20:50:52 +0800

    *crying like a little baby* WAH WAH

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -4,4 +4,5 @@ var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
 var image = "https://mechagic.party/2images/3tjarparchivist.webp";
 
-document.querySelector('#siwebring #templateWebring').style = "font-family: trebuchet ms, sans-serif;     text-align: center;     margin:auto;     width: 85%!important;     height: auto!important;     background: url('/siwebring/siwidgetbg.png') no-repeat;     background-size: cover;     border: 3px #FF74B8 solid;     font-size:%2016px;";
+$("templateWebring").css("width","85%"),
+$("templateWebring").css("height","auto");
