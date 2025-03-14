38930af081b8f528db3280b09218157b57136116
mechagic <75117749+mechagic@users.noreply.github.com>
2025-03-14 21:08:50 +0800

    june is a blessing to humanity

--- src/1stylescripts/siwebringuservars.js
+++ src/1stylescripts/siwebringuservars.js
@@ -4,7 +4,3 @@ var thing = "having a joyous PLATONIC time";
 var character = "The Archivist";
 var media = "The June Archive and Restoration Project";
 var image = "https://mechagic.party/2images/3tjarparchivist.webp";
-
-/*webring custom css*/
-$("templateWebring").css("width","85%"),
-$("templateWebring").css("height","auto");
